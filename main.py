88b5425457c469a0bc30fa28ef3a1e1b5c1f8720
Jonathan Guichard <jona.guich69@gmail.com>
2021-05-08 14:10:33 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import obd
+
+connection = obd.OBD()  # auto-connects to USB or RF port
+
+cmd = obd.commands.SPEED  # select an OBD command (sensor)
+
+response = connection.query(cmd)  # send the command, and parse the response
+
+print(response.value)  # returns unit-bearing values thanks to Pint
+print(response.value.to("kmh"))  # user-friendly unit conversions
+
